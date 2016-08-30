bc129288db2729380390a5bfa551bc039708ad19
Katriel Cohn-Gordon <katriel.cohn-gorodn@cybersecurity.ox.ac.uk>
2016-08-30 09:38:44 +0100

    Fix relative path to deploy key.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+# Set up some git information.
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
@@ -14,11 +19,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     doCompile
     exit 0
 fi
-
-# Set up some git information.
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into a temporary folder $CHECKOUT.
 CHECKOUT=`mktemp -d`
@@ -40,17 +40,15 @@ if [[ -z `git -C $CHECKOUT status --porcelain` ]]; then
     exit 0
 fi
 
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git -C $CHECKOUT add .
-git -C $CHECKOUT commit -m "Deploy to GitHub Pages: ${SHA}"
+# Commit the "changes", i.e. the new version. The delta will show diffs between new and old versions.
+git -C $CHECKOUT commit -am "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc.
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
